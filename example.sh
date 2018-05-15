88e00c44345bbcebf08ceb74e5dbc9bc8bbaa509
kadora <k.kadora@gmail.com>
2018-05-15 21:20:01 +0900

    fix example.sh

--- example.sh
+++ example.sh
@@ -4,7 +4,7 @@
 python txt2qubo.py -t input.txt -o partion.qubo
 
 # Execute qbsolv
-./qbsolv -i partion.qubo -o partion.qbout -v1 -n 8
+qbsolv -i partion.qubo -o partion.qbout -v1 -n 8
 
 # Output result of problem
 python result.py -i input.txt -s partion.qbout
